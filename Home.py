85c87596e368ca36f87d2704f91a88cbc4f350d4
Kan Fu <fukan.thu@gmail.com>
2025-11-21 11:38:49 -0800

    oncdw dashboard structured

--- Home.py
+++ Home.py
@@ -3,20 +3,29 @@ from oncdw import ONCDW
 
 client = ONCDW() 
 
-with st.sidebar:
-    client.ui.device_sidebar({"device_id": 21501, "device_code": "BPR-1027NW"})
-    client.ui.sensor_sidebar({"sensor_id": 4182, "sensor_name": "Seafloor Pressure"})
-    client.ui.sensor_sidebar(
-        {"sensor_id": 7712, "sensor_name": "Uncompensated Seafloor Pressure"}
-    )
-    client.ui.device_sidebar({"device_id": 12501, "device_code": "BPR_BC"})
-    client.ui.sensor_sidebar({"sensor_id": 4176, "sensor_name": "Seafloor Pressure"})
+devices = [
+    {
+        "device_id": 21501,
+        "device_code": "BPR-1027NW",
+        "sensors": [
+            {"sensor_id": 4182, "sensor_name": "Seafloor Pressure"},
+            {"sensor_id": 7712, "sensor_name": "Uncompensated Seafloor Pressure"},
+        ],
+    },
+    {
+        "device_id": 12501,
+        "device_code": "BPR_BC",
+        "sensors": [{"sensor_id": 4176, "sensor_name": "Seafloor Pressure"}],
+    },
+]
 
-client.ui.device({"device_id": 21501, "device_code": "BPR-1027NW"})
-sensor1 = {"sensor_id": 4182, "sensor_name": "Seafloor Pressure"}
-client.section.time_series(sensor1)
-sensor2 = {"sensor_id": 7712, "sensor_name": "Uncompensated Seafloor Pressure"}
-client.section.time_series(sensor2)
-client.ui.device({"device_id": 12501, "device_code": "BPR_BC"})
-sensor1 = {"sensor_id": 4176, "sensor_name": "Seafloor Pressure"}
-client.section.time_series(sensor1)
+with st.sidebar:
+    for device in devices:
+        client.ui.device(device)
+        for sensor in device["sensors"]:
+            client.section.sensor_sidebar(sensor)
+
+for device in devices:
+    client.ui.device(device)
+    for sensor in device["sensors"]:
+        client.section.time_series(sensor)
